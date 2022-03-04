fbcc17bee608bb328f824a627aec5c44dd6d6b7e
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-03-04 18:49:04 +0100

    Learnt how to consume and create API in Express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-const port = 5000;
-
+/*
 // Create Route
 app.get('/', (req, res) => {
   //   res.status(200).send('Hello world...at the root');
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     password: '091683Anu',
     email: 'oderinloanuoluwapo@gmail.com',
     result: {
-      cgpa: 4.88,
+      cgpa: '4.88',
       'course-title': 'Cyber war',
       'course-code': 'cy-111',
     },
@@ -23,7 +23,44 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
 });
+*/
 
+app.use(express.json()); //this serves as middleware
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: newTour,
+      });
+    }
+  );
+});
+
+const port = 5000;
 // listening to server
 app.listen(port, () => {
   console.log(`App listening at port ${port}`);
