118413e636a27ae4358c5257668eb252683c9f9d
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-04-11 11:06:13 +0100

    Implemeted the update user data API

--- 4-natours/starter/controller/userController.js
+++ 4-natours/starter/controller/userController.js
@@ -1,6 +1,17 @@
 const User = require('./../model/userModel');
 // const APIFeatures = require('./../utils/apiFeatures');
+const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -13,6 +24,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.user.id);
+  // 1. If you users POST password data, send error
+  if (req.body.password || req.body.passwordConfirm) {
+    next(
+      new AppError(
+        'Wrong route for password update, use /updatePassword route',
+        400
+      )
+    );
+  }
+
+  // 2. Update user data
+
+  const filterBody = filterObj(req.body, 'name', 'email');
+
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  // response
+  res.status(200).json({
+    status: 'success',
+    user: updateUser,
   });
 });
 
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -14,6 +14,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 // Users
 router
   .route('/')
